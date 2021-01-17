d064067d70bfcf266d9d6809e85122f5674c5cc6
Anurag Das <anurag.das23.ad@gmail.com>
2021-01-17 18:20:06 -0500

    Added register page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import React from 'react';
@@ -80,17 +81,20 @@ class App extends React.Component {
               params={particlesOptions}
       />
       <Navigation onRouteChange={this.onRouteChange}/>
-      { this.state.route === 'signin' 
-      ?<Signin onRouteChange={this.onRouteChange}/>
-      :<div>
-          <Logo />
-          <Rank />
-          <ImageLinkForm 
-              onInputChange={this.onInputChange} 
-              onButtonSubmit={this.onButtonSubmit}/>
-          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+      { this.state.route === 'home' 
+      ?<div>
+      <Logo />
+      <Rank />
+      <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}/>
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
-      }
+      :(
+        this.state.route === 'signin' 
+      ? <Signin onRouteChange={this.onRouteChange}/>
+      : <Register onRouteChange={this.onRouteChange}/>
+      )}
     </div>
   );
   };
