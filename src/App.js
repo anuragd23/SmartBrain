d6fb18cddef5c114efb07a9e07b0b375e2d117da
Anurag Das <anurag.das23.ad@gmail.com>
2021-02-16 19:05:15 -0500

    Adding user validation and moving API key to backend

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import React from 'react';
-
-
-const app = new Clarifai.App({
-  apiKey: '45eb83266af14460987ef3544bc663d2'
-});
-
 
 const particlesOptions = {
   particles: {
@@ -83,9 +76,17 @@ class App extends React.Component {
   }
 
   onPictureSubmit = () => {
-    this.setState({imageUrl: this.state.input})
-    app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input)
-    .then(response => {
+    this.setState({imageUrl: this.state.input});
+
+    fetch('http://localhost:3000/imageurl', {
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            input: this.state.input         
+          })
+      })
+      .then(response => response.json())
+      .then(response => {
       if(response) {
         fetch('http://localhost:3000/image', {
           method: 'put',
